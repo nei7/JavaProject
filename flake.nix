8a2a4a783179232a7b34060edffb46b60e360733
nei7 <franciszek.szarek2@gmail.com>
2026-03-13 18:06:40 +0100

    chore: inital commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Java project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+
+        buildInputs = with pkgs; [
+
+          jdk25_headless
+          maven
+        ];
+
+        shellHook = ''
+          export JAVA_HOME=${pkgs.jdk25_headless}
+        '';
+      };
+    };
+}
